a2ff57382b56c5129acb6ee837f434337c7235f9
James <jimmycmcleod@gmail.com>
2022-02-02 18:55:52 -0500

    A4 Skins upload

---
+++ A-4E-C/USN VA-103/description.lua
@@ -0,0 +1,64 @@
+-- rename it to description.lua
+livery =
+{
+    {"Bottom", 0, "va103_bot", false};
+    {"Top", 0, "va103_top", false};
+
+        -- flaps, used on most liveries
+    {"flaps_001", 0, "empty", true};
+    {"flaps_010", 0, "empty", true};
+    {"flaps_100", 0, "empty", true};
+
+    -- three digit, smaller nose numbers, used on most liveries
+    {"nose_001", 0, "empty", true};
+    {"nose_010", 0, "empty", true};
+    {"nose_100", 0, "empty", true};
+
+    -- two digit top-of-tail, used on most USMC liveries
+    {"tail_aggressor_001", 0, "empty", true};
+    {"tail_aggressor_010", 0, "empty", true};
+
+    -- three digit top-of-rudder, used on most USN liveries
+    {"rudder_001", 0, "empty", true};
+    {"rudder_010", 0, "empty", true};
+    {"rudder_100", 0, "empty", true};
+
+    -- wing bort used on most USN liveries
+    {"wing_001", 0, "empty", true};
+    {"wing_010", 0, "empty", true};
+    {"wing_100", 0, "empty", true};
+
+    -- 2-digit large nose numbers, used on USN/USMC aggressors
+    {"nose_aggressor_001", 0, "empty", true};
+    {"nose_aggressor_010", 0, "empty", true};
+
+    -- 3-digit large tail numbers, used on some USN/USMC aggressors
+    {"tail_001", 0, "empty", true};
+    {"tail_010", 0, "empty", true};
+    {"tail_100", 0, "empty", true};
+
+    -- custom fuel tanks: 150 gallon
+    {"fuel_150_a4e", 0, "va103_dt150_300", false};
+    {"fuel_150_a4e", 2, "va103_dt150_300", false};
+    {"fuel_150_a4e", 12, "va103_dt150_300", false};
+
+    -- custom fuel tank: 300 gallon left/right
+    {"fuel_300lr_a4e", 0, "va103_dt150_300", false};
+    {"fuel_300lr_a4e", 2, "va103_dt150_300", false};
+    {"fuel_300lr_a4e", 12, "va103_dt150_300", false};
+
+    -- custom fuel tank: 300 gallon center
+    {"fuel_300c_a4e", 0, "va103_dt150_300", false};
+    {"fuel_300c_a4e", 2, "va103_dt150_300", false};
+    {"fuel_300c_a4e", 12, "va103_dt150_300", false};
+
+    -- custom fuel tank: 400 gallon
+    {"fuel_400_a4e", 0, "va103_dt400", false};
+    {"fuel_400_a4e", 2, "va103_dt400", false};
+    {"fuel_400_a4e", 12, "va103_dt400", false};
+
+}
+
+name = "USN VA-103"
+--name_xx = ""
+countries = {"USA"}
