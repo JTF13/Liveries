b1c7708209b0662ea29ce859a670b63c94acbfd7
Vishari <47961951+Vishari@users.noreply.github.com>
2022-02-02 18:39:03 -0500

    Initial Skin Commit

---
+++ mig-23mld/SAAF/description.lua
@@ -0,0 +1,14 @@
+livery = {
+
+{"texture MIG-23-PAINT-DEF1.bmp f2 g0",	DIFFUSE			,	"MIG-23-PAINT-DEF3", false};
+{"texture mig-23-paint-def1.bmp f2050 g0",	DIFFUSE			,	"MIG-23-PAINT-DEF3", false};
+
+{"texture RUS-STAR-ZNAK.TGA f2 g0", 0, "empty", true};
+{"texture UKR-KRUG.tga f2 g0", 0, "empty", true};
+{"texture UKR-LOGO.tga f2 g0", 0, "empty", true};
+{"texture rus-code-numbers-ver1-r-w-w.tga f2 g0", 0, "empty", true};
+{"texture rus-pilot-2.bmp f2 g0", 0, "RUS-PILOT-2-AAF.dds", true};
+}
+name = "SAAF"
+countries = {	"SYR"   }
+
