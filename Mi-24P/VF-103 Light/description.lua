4f9a1e4dfc4c619e69251c704c62402c87805a67
James <jimmycmcleod@gmail.com>
2022-02-02 19:58:22 -0500

    UH-1H/Mi-24/T-45 Skins upload

---
+++ Mi-24P/VF-103 Light/description.lua
@@ -0,0 +1,73 @@
+livery = {
+
+-- mi-24p_tex01 - temporary
+{"mi-24p_tex01_clear", 0, "mi-24p_tex01",	true};
+{"mi-24p_tex01_clear", 1, "mi-24p_tex01_NRM",	true};
+{"mi-24p_tex01_clear", ROUGHNESS_METALLIC, "mi-24p_tex01_RoughMet", true};
+{"mi-24p_blade_no_shadow",	DIFFUSE			,	"mi-24p_blade", false};
+{"mi-24p_blade_shad",	DIFFUSE			,	"mi-24p_blade", false};
+{"BD3-57",	DIFFUSE			,	"bd3-57_diff", false};
+
+-- mi-24p_tex03
+  {"mi-24p_tex03", 0,	"mi-24p_tex03", true};
+  {"mi-24p_tex03", 1,	"mi-24p_tex03_NRM",	true};
+  {"mi-24p_tex03", ROUGHNESS_METALLIC, "mi-24p_tex03_RoughMet", true};
+
+  {"mi-24p_tex03_clear", 0,	"mi-24p_tex03", true};
+  {"mi-24p_tex03_clear", 1,	"mi-24p_tex03_NRM",	true};
+  {"mi-24p_tex03_clear", ROUGHNESS_METALLIC, "mi-24p_tex03_RoughMet", true};
+
+-- mi-24p_ext01
+  {"mi-24p_ext01", 0, "mi-24p_ext01_un1", false};
+  {"mi-24p_ext01", 1, "mi_24p_ext01_NRM",	true};
+  {"mi-24p_ext01", ROUGHNESS_METALLIC, "mi-24p_ext01_RoughMet", true};
+
+-- mi-24p_ext02
+  {"mi-24p_ext02", 0, "mi-24p_ext02_un1", false};
+  {"mi-24p_ext02", 1, "mi_24p_ext02_NRM",	true};
+  {"mi-24p_ext02", ROUGHNESS_METALLIC, "mi-24p_ext02_RoughMet", true};
+
+-- mi-24p_ext03
+  {"mi-24p_ext03", 0, "mi-24p_ext03_un1", false};
+  {"mi-24p_ext03", 1, "mi_24p_ext03_NRM",	true};
+  {"mi-24p_ext03", ROUGHNESS_METALLIC, "mi-24p_ext03_RoughMet", true};
+
+-- mi-24p_ext04
+  {"mi-24p_ext04", 0, "mi-24p_ext04_un1", false};
+  {"mi-24p_ext04", 1, "mi_24p_ext04_NRM",	true};
+  {"mi-24p_ext04", ROUGHNESS_METALLIC, "mi-24p_ext04_RoughMet", true};
+
+-- mi-24p_ext05
+  {"mi-24p_ext05", 0, "mi-24p_ext05_un1", false};
+  {"mi-24p_ext05", 1, "mi_24p_ext05_NRM",	true};
+  {"mi-24p_ext05", ROUGHNESS_METALLIC, "mi-24p_ext05_RoughMet", true};
+
+-- mi-24p_ext06
+  {"mi-24p_ext06", 0, "mi-24p_ext06_un1", false};
+  {"mi-24p_ext06", 1, "mi_24p_ext06_NRM",	true};
+  {"mi-24p_ext06", ROUGHNESS_METALLIC, "mi-24p_ext06_RoughMet", true};
+
+  {"mi-24p_ext06_BN",	0,	                "mi-24p_ext06_un1", false};
+  {"mi-24p_ext06_BN",	1,	                "mi_24p_ext06_NRM",		true};
+  {"mi-24p_ext06_BN",	ROUGHNESS_METALLIC,	"mi-24p_ext06_RoughMet",	true};
+  {"mi-24p_ext06_BN",	DECAL,	            "empty",	true};
+
+-- mi-24p_ext07
+  {"mi-24p_ext07", 0, "mi-24p_ext07_un1", false};
+  {"mi-24p_ext07", 1, "mi_24p_ext07_NRM",	true};
+  {"mi-24p_ext07", ROUGHNESS_METALLIC, "mi-24p_ext07_RoughMet", true};
+
+-- mi-24p_ext07_net
+  {"mi-24p_ext07_net", DECAL, "mi-24p_ext07_un1", false};
+  {"mi-24p_tex03_clear",	DIFFUSE , "mi-24p_tex03_old", false};
+  {"9m114_pylon",	DIFFUSE			,	"9m114-pylon_diff", false};
+
+-- Pilot
+
+}
+
+
+
+name = "VF-103 Light"
+
+order     = 1
---
+++ T-45/JTF13 FAM 020 Bryan - Flash/description.lua
@@ -0,0 +1,44 @@
+livery = {
+	--{"mirror", 0 ,"mirrors",true};--mirrors texture replacement with runtime rendered one
+	
+-------------------------------------------------------------------------------------------------
+
+	 {"T45_External_01", 0 , "T45_External_01_DIF", false};
+--	 {"T45_External_01", ROUGHNESS_METALLIC , "T45_External_01_DIF_RoughMet", false};
+	 {"T45_External_02", 0 , "T45_External_02_DIF", false};
+--	 {"T45_External_02", ROUGHNESS_METALLIC , "T45_External_02_DIF_RoughMet", false};
+	-- {"T45_External_03", 0 , "T45_External_03_DIF", false};
+	-- {"T45_External_04", 0 , "T45_External_04_DIF", false};
+	 {"T45_Canopy", 0 , "T45_Canopy_DIF", false};
+  
+	
+------------BORT NUMBERS-------------------------------------------------------------------------
+
+	 {"T45_L_Bort_Digit_1s", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_L_Bort_Digit_10s", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_L_Bort_Digit_100s", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_R_Bort_Digit_1s", 0 , "T45_bort_number_RIGHT_TF", false};
+	 {"T45_R_Bort_Digit_10s", 0 , "T45_bort_number_RIGHT_TF", false};
+	 {"T45_R_Bort_Digit_100s", 0 , "T45_bort_number_RIGHT_TF", false};
+
+	 {"T45_L_Bort_Digit_Static", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_R_Bort_Digit_Static", 0 , "T45_bort_number_RIGHT_TF", false};
+
+	 {"T45_L_Bort_Digit_Static_100", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_L_Bort_Digit_Static_10", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_L_Bort_Digit_Static_1", 0 , "T45_bort_number_LEFT_TF", false};
+	 {"T45_R_Bort_Digit_Static_100", 0 , "T45_bort_number_RIGHT_TF", false};
+	 {"T45_R_Bort_Digit_Static_10", 0 , "T45_bort_number_RIGHT_TF", false};
+	 {"T45_R_Bort_Digit_Static_1", 0 , "T45_bort_number_RIGHT_TF", false};
+
+-------------------------------------------------------------------------------------------------	
+-------------------------------------------------------------------------------------------------
+	
+	-- {"NP_Helmet_Shell_Front", 0 , "NP_Helmet_Shell_Front_DIF", false};
+	-- {"NP_VisorShield_Front", 0 , "NP_VisorShield_Front_DIF", false};
+	-- {"NP_Helmet_Shell_Rear", 0 , "NP_Helmet_Shell_Rear_DIF", false};
+	-- {"NP_VisorShield_Rear", 0 , "NP_VisorShield_Rear_DIF", false};	
+	
+}
+name = "JTF-13 FAM BGen Bryan - Flash"
+-- countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
