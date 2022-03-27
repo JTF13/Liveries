3cc757ce0798eca195e2ab20093c8bd0e68297d3
null <null@0x36a.com>
2022-03-26 17:06:48 -0700

    Unifying Livery Naming Conventions for VF-103

---
+++ F-14B/VF-103 - 106 - Dory/description.lua
@@ -0,0 +1,29 @@
+livery = {
+
+
+--Diffuse  -----------------------------------
+
+{"HB_F14_EXT_01", 0 ,"HB_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"HB_F14_EXT_02",false};
+{"HB_F14_EXT_03", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_03",false};
+{"HB_F14_EXT_04", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_04",false};
+{"HB_F14_EXT_TAIL", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_TAIL",false};
+{"HB_F14_WING_LEFT_01", 0 ,"../VF-103 - 000 - Line/HB_F14_WING_LEFT",false};
+{"HB_F14_WING_RIGHT", 0 ,"../VF-103 - 000 - Line/HB_F14_WING_RIGHT",false};
+{"HB_F14_LOD1_3in1", 0 ,"../VF-103 - 000 - Line/HB_F14_LOD1_3in1",false};
+{"HB_F14_LOD3_3in1", 0 ,"../VF-103 - 000 - Line/HB_F14_LOD3_3in1",false};
+{"HB_F14_EXT_PILOT_HELMET", 0 ,"../VF-103 - 000 - Line/Helmets/Custom/JTF_F14_HELMET_DORY",false};
+{"HB_F14_EXT_PILOT_HELMET", 1 ,"../VF-103 - 000 - Line/Helmets/Line/JTF_F14_HELMET_NORMAL_MAP",false};
+{"HB_F14_EXT_PILOT_HELMET", ROUGHNESS_METALLIC ,"../VF-103 - 000 - Line/Helmets/Line/JTF_F14_HELMET_ROUGHMET",false};
+{"HB_F14_EXT_RIO_HELMET", 0 ,"../VF-103 - 000 - Line/Helmets/Custom/JTF_F14_HELMET_ARK",false};
+{"HB_F14_EXT_RIO_HELMET", 1 ,"../VF-103 - 000 - Line/Helmets/Line/JTF_F14_HELMET_NORMAL_MAP",false};
+{"HB_F14_EXT_RIO_HELMET", ROUGHNESS_METALLIC ,"../VF-103 - 000 - Line/Helmets/Line/JTF_F14_HELMET_ROUGHMET",false};
+{"HB_F14_EXT_PILOT_SUIT", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_PILOT_SUIT_CDR",false};
+{"HB_F14_EXT_PILOT_SUIT", 1 ,"../VF-103 - 000 - Line/HB_F14_EXT_PILOT_SUIT_NRM",false};
+{"HB_F14_EXT_PILOT", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_PILOT",false};
+{"HB_F14_EXT_DROPTANKS", 0 ,"../VF-103 - 000 - Line/HB_F14_EXT_DROPTANK",false};
+
+}
+name = "VF-103 - 106 - Dory"
+
+--countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
