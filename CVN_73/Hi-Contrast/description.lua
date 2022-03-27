f3d2824a024a61d0adcc3ff48a804613c021521c
Loot <Loren@fullspektrum.com>
2022-03-27 14:07:00 -0700

    Navy QOL Updates plus hotfix for 143

---
+++ CVN_73/Hi-Contrast/description.lua
@@ -0,0 +1,12 @@
+livery = {
+
+{"Add_Shield",	DIFFUSE			,	"add_shield_base", false};
+{"Details",	DIFFUSE			,	"nimitz_details_base", false};
+{"Material #11",	DIFFUSE			,	"Deck_Base", false};
+{"Material #11",	DECAL				,	"Nimitz_Deck_Decal_Base", false};
+{"Material #11",	DIRT				,	"Nimitz_Deck_Decal_Base_RoughMet", false};
+{"Nimitz_Props",	DIFFUSE			,	"nimitz_props_base", false};
+
+}
+name = "Hi-Contrast"
+countries = {"USA"}
---
+++ SH-60 Seahawk/description.lua
@@ -0,0 +1,9 @@
+--Livery made by PorcoRosso86
+
+livery = {
+	{"uh60", 0, "uh60_texture_USN", false}; -- main material for lod0 lod1
+	{"uh60 pilons", 0, "uh60_texture_UN", true};  -- main material for lod2 lod3
+}
+name = "US Navy 'Carl Vinson"
+countries = { "USA",}
+
