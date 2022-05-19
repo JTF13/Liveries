5a71094197e6250d5011f508c7a22cf061c3cab9
null <null@0x36a.com>
2022-05-19 10:09:54 -0700

    Updating Nulls 14A livery

--- F-14A-135-GR/VF-103 - 107 - Null/description.lua
+++ F-14A-135-GR/VF-103 - 107 - Null/description.lua
@@ -1,12 +1,12 @@
 livery = {
 
-{"HB_F14_EXT_01",             0,                    "../../f-14b/VF-103 - 107 - Ribbit/HB_F14_EXT_01",                            false};
-{"HB_F14_EXT_02",             0,                    "../../f-14b/VF-103 - 107 - Ribbit/HB_F14_EXT_02",                            false};
+{"HB_F14_EXT_01",             0,                    "../../f-14b/VF-103 - 107 - Null/HB_F14_EXT_01",                              false};
+{"HB_F14_EXT_02",             0,                    "../../f-14b/VF-103 - 107 - Null/HB_F14_EXT_02",                              false};
 {"HB_F14_EXT_03",             0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_EXT_03",                              false};
 {"HB_F14_EXT_04",             0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_EXT_04",                              false};
-{"HB_F14_EXT_TAIL",           0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_EXT_TAIL",                            false};
-{"HB_F14_WING_LEFT_01",       0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_WING_LEFT",                           false};
-{"HB_F14_WING_RIGHT",         0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_WING_RIGHT",                          false};
+{"HB_F14_EXT_TAIL",           0,                    "../../f-14b/VF-103 - 107 - Null/HB_F14_EXT_TAIL",                            false};
+{"HB_F14_WING_LEFT_01",       0,                    "../../f-14b/VF-103 - 107 - Null/HB_F14_WING_LEFT",                           false};
+{"HB_F14_WING_RIGHT",         0,                    "../../f-14b/VF-103 - 107 - Null/HB_F14_WING_RIGHT",                          false};
 {"HB_F14_LOD1_3in1",          0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_LOD1_3in1",                           false};
 {"HB_F14_LOD3_3in1",          0,                    "../../f-14b/VF-103 - 000 - Line/HB_F14_LOD3_3in1",                           false};
 {"HB_F14_EXT_PILOT_HELMET",   0,                    "../../f-14b/VF-103 - 000 - Line/Helmets/Custom/JTF_F14_HELMET_NULL",         false};
