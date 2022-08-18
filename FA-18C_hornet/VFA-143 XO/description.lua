c04cbaf066a183924a01d01bc522bba5b2a4d7b7
xWhiskeysx <jschenemann@gmail.com>
2022-08-17 22:31:08 -0400

    Update for All 143 Hornet Skinz
    Hornet Skins updated and added

--- FA-18C_hornet/VFA-143 XO/description.lua
+++ FA-18C_hornet/VFA-143 XO/description.lua
@@ -10,7 +10,7 @@ livery = {
 	{"pilot_F18_patch", 0 ,"Pilot_F18_Patch_PukinDogs",false};
 	{"pilot_F18_patch", 1 ,"Pilot_F18_Patch_NORM_PukinDogs",false};
 	{"pilot_F18_patch", ROUGHNESS_METALLIC ,"Pilot_F18_Patch_PukinDogs_RoughMet",false};
-	{"pilot_F18_helmet", 0, "../VFA-143Textures/Helmets/PILOT_F18_HELMET_LOOT", false};
+	{"pilot_F18_helmet", 0, "../VFA-143Textures/PILOT_F18_HELMET_VFA_DEFAULT", false};
 	
 	--Fuel Tanks
 	{"FPU_8A", 0 ,"FPU_8A_PukinDogs_XO",false};
