af6031fc3792432887abfc29c4b61d3eb29c72a2
null <null@0x36a.com>
2022-08-25 16:33:35 -0700

    16 update for Flash.

---
+++ F-16C_50/JTF-13 93rd FS HG  Maj Gen Bryan - Flash/description.lua
@@ -0,0 +1,106 @@
+livery = {
+
+
+
+    {"F16_bl50_Main_1", DIFFUSE, "f16_bl50_main_1", false};
+	{"F16_bl50_Main_1", 1, "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_normal", false};
+	{"F16_bl50_Main_1",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_roughmet", false};
+  
+	{"F16_bl50_Main_2", DIFFUSE, "../JTF-13 93rd FS HG-Textures/f16_bl50_main_2", false};
+	{"F16_bl50_Main_2", 1, "../JTF-13 93rd FS HG-Textures/f16_bl50_main_2_normal", false};
+	{"F16_bl50_Main_2",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_2_roughmet", false};
+
+	{"F16_bl50_Main_3", DIFFUSE, "../JTF-13 93rd FS HG-Textures/f16_bl50_main_3", false};
+	{"F16_bl50_Main_3", 1, "../JTF-13 93rd FS HG-Textures/f16_bl50_main_3_normal", false};
+   	{"F16_bl50_Main_3",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_3_roughmet", false};
+	
+	{"F16_bl50_Kil", DIFFUSE, "../JTF-13 93rd FS HG-Textures/f16_bl50_kil", false};
+	{"F16_bl50_Kil",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_kil_roughmet", false};
+
+   	{"F16_bl50_wing_L", DIFFUSE, "../JTF-13 93rd FS HG-Textures/f16_bl50_wing_l", false};
+	{"F16_bl50_wing_L", 1, "../JTF-13 93rd FS HG-Textures/f16_bl50_wing_l_normal", false};
+   	{"F16_bl50_wing_L",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_wing_l_roughmet", false};
+
+	{"F16_bl50_wing_R", DIFFUSE, "../JTF-13 93rd FS HG-Textures/f16_bl50_wing_r", false};
+	{"F16_bl50_wing_R", 1, "../JTF-13 93rd FS HG-Textures/f16_bl50_wing_r_normal", false};
+  	{"F16_bl50_wing_R",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_wing_r_roughmet", false};
+
+
+	{"F16_bl50_Wing_Pylon_1", 0 ,"../JTF-13 93rd FS HG-Textures/F16_bl50_Wing_Pylon_1",false};
+	{"F16_bl50_Wing_Pylon_2", 0 ,"../JTF-13 93rd FS HG-Textures/F16_bl50_Wing_Pylon_2",false};
+	{"LAU_129", 0 ,"../JTF-13 93rd FS HG-Textures/LAU_129_diff",false};
+
+	{"Fuel_Tank_300Gal",	DIFFUSE			,	"../JTF-13 93rd FS HG-Textures/fuel_tank_300gal", false};
+	{"Tank_370", DIFFUSE, "../JTF-13 93rd FS HG-Textures/fuel_tank_370gal_diff", false};
+
+	{"F16_bl50_Engine", 0, "../JTF-13 93rd FS HG-Textures/f16_bl50_engine", false};
+	{"F16_bl50_Engine", ROUGHNESS_METALLIC ,"../JTF-13 93rd FS HG-Textures/F16_bl50_Engine_RoughMet",false};
+
+	{"pilot_F16", 0, "../JTF-13 93rd FS HG-Textures/pilot_F16", false};
+	{"pilot_F16_helmet", 0, "pilot_F16_helmet",false};
+	{"pilot_F16_patch", 0 ,"../JTF-13 93rd FS HG-Textures/pilot_F16_patch",false};
+	{"pilot_F16_patch", ROUGHNESS_METALLIC ,"../JTF-13 93rd FS HG-Textures/pilot_F16_patch_roughmet",false};
+	{"pilot_F16_patch", 1 ,"../JTF-13 93rd FS HG-Textures/pilot_F16_patch_normal",false};
+
+	{"PTB_300Gal", 0, "../JTF-13 93rd FS HG-Textures/fuel_tank_300gal", false};
+	{"PTB_300Gal", 1, "../JTF-13 93rd FS HG-Textures/Fuel_Tank_300Gal_Normal", false};
+	{"PTB_300Gal", ROUGHNESS_METALLIC, "../JTF-13 93rd FS HG-Textures/Fuel_Tank_300Gal_RoughMet", false};
+
+	{"F_16_Tank_370",	DIFFUSE			,	"../JTF-13 93rd FS HG-Textures/fuel_tank_370gal_diff", false};
+
+	
+--BORT_NUMBER----------------------------------------------------------------------------------------
+
+
+
+--F16_bl50_FIN_DECAL
+    {"F16_bl50_FIN_DECAL",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_kil",				false};
+	{"F16_bl50_FIN_DECAL",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_kil_normal",		true};
+	{"F16_bl50_FIN_DECAL",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_kil_roughmet", false};
+	{"F16_bl50_FIN_DECAL",	DECAL,	            "../JTF-13 93rd FS HG-Textures/F16_AF_Decal", false};
+
+--F16_bl50_FIN_BORT_NUMBER
+    {"F16_bl50_FIN_BORT_NUMBER",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_kil",				false};
+	{"F16_bl50_FIN_BORT_NUMBER",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_kil_normal",		true};
+	{"F16_bl50_FIN_BORT_NUMBER",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_kil_roughmet", false};
+	{"F16_bl50_FIN_BORT_NUMBER",	DECAL,	                  "../JTF-13 93rd FS HG-Textures/F16_bort_number", false};
+
+
+--F16_bl50_NOUSE_DECAL
+    {"F16_bl50_MAT1_BORT_NUMBER_X100",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	DECAL,	            "../JTF-13 93rd FS HG-Textures/F16_bort_number", false};
+
+--F16_bl50_REFUEL_DECAL
+    {"F16_bl50_MAT1_BORT_NUMBER_DECAL",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	DECAL,	            "../JTF-13 93rd FS HG-Textures/F16_AF_Decal", false};
+
+--F16_bl50_NOUSE AND REFUEL BORT_NUMBER
+    {"F16_bl50_MAT1_BORT_NUMBER",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	DECAL,	            "../JTF-13 93rd FS HG-Textures/F16_bort_number", false};
+
+
+--F16_bl50_INTAKE_BORT_NUMBER
+    {"F16_bl50_INTAKE_BORT_NUMBER",	0,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_3",				false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	1,	                "../JTF-13 93rd FS HG-Textures/f16_bl50_main_3_normal",		false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	ROUGHNESS_METALLIC,	"../JTF-13 93rd FS HG-Textures/f16_bl50_main_3_roughmet", false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	DECAL,	            "empty",	true};
+}
+
+
+
+--name = "JTF-13 93rd FS Makos HG Maj Gen Bryan - Flash"
+countries = {}
+
+custom_args = 
+{
+[1000] = 0.0, -- change of type of board number (0.0 -default USA, 0.1- )
+[1001] = 0.0, -- vis refuel board number 
+[1002] = 1.0, -- change of type intake board number 
+[1003] = 1.0, -- vis nouse board number 
+}
