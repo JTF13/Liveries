6a10d6bc979c819d53be431d75b1b3e09e225c39
Stlshady <90146008+Stlshady@users.noreply.github.com>
2023-03-12 22:53:44 -0500

    Updates to 224 and 143 Hornet liveries
    names / ranks / Modex etc.  cleaned up some jets.

---
+++ F-5E-3/PAK-FA/description.lua
@@ -0,0 +1,47 @@
+livery = {
+	{"F-5E3_bort_number_nose_100", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_bort_number_nose_100", DECAL ,"empty",true};
+	{"F-5E3_bort_number_nose_10", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_bort_number_nose_10", DECAL ,"empty",true};
+	{"F-5E3_bort_number_nose_1", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_bort_number_nose_1", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_nose_10A", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_bort_number_nose_10A", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_nose_1A", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_bort_number_nose_1A", DECAL ,"empty",true};
+	
+	
+	{"F-5E3_bort_number_rude_100", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_bort_number_rude_100", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_rude_10", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_bort_number_rude_10", DECAL ,"empty",true};
+	{"F-5E3_bort_number_rude_10A", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_bort_number_rude_10A", DECAL ,"empty",true};	
+	
+	{"F-5E3_bort_number_rude_1", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_bort_number_rude_1", DECAL ,"empty",true};
+	{"F-5E3_bort_number_rude_1A", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_bort_number_rude_1A", DECAL ,"empty",true};
+	
+	
+	{"F-5E3_nose", 0 ,"VFC-111_121_f-5e3_nose",false};
+	{"F-5E3_cockpit", 0 ,"F-5e3_cockpit",true};
+	{"F-5E3_tail", 0 ,"VFC-111_121_f-5e3_tail",false};
+	{"F-5E3_rude", 0 ,"VFC-111_121_f-5e3_rude",false};
+	{"F-5E3_wing", 0 ,"VFC-111_121_f-5e3_wing",false};
+	
+	{"pilot_F5_E3", 0 ,"../Agressor_VMFT-401_02/VMFT-401 pilot_f-5e3",false};
+	{"pilot_F5E3_helmet", 0 ,"f-5e3_VFC-111 sundowners_helmet",false};
+	{"pilot_F15_patch", 0 ,"VFC-111_PilotPatch",false};
+}
+name = "Sundowners_VFC-111_121_Sundowners"
+countries = {"IND", "RUS", "UKR", "USA", "TUR", "UK", "FRA", "GER", "AUSAF",
+             "SPN", "BEL", "NOR", "DEN", "ISR", "GRG", "INS", "ABH", "NETH",
+             "RSO", "ITA", "AUS", "SUI", "AUT", "BLR", "BGR", "CZE", "CHN",
+             "HRV", "EGY", "FIN", "GRC", "HUN", "CAN", "IRN", "IRQ", "JPN",
+             "KAZ", "PRK", "PAK", "POL", "ROU", "SAU", "SRB", "SVK", "KOR",
+             "SWE", "SYR"}
+-- made by Hocician
