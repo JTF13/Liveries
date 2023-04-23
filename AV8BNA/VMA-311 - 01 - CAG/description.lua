36391d499a789a4d16abde184c80c4182894ff4e
Stlshady <90146008+Stlshady@users.noreply.github.com>
2023-04-23 18:03:34 -0500

    Added CAG harrier livery
    VMA-311 #01 - Apollo, Void

---
+++ AV8BNA/VMA-311 - 01 - CAG/description.lua
@@ -0,0 +1,52 @@
+livery = {
+
+	{"AV8B1", 0 ,"AV8_T1_VMA311_CAG",false};
+	{"AV8B2", 0 ,"AV8_T2_VMA311_CAG",false};
+	{"AV8B3", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B7", 0 ,"AV8_T7_VMA311_CAG",false};
+	
+	{"pilot_AV8B_body", 0 ,"pilot_AV8B",true};
+	{"pilot_AV8B_patch", 0 ,"pilot_AV8B_patch_VMA311_CAG",false};
+	{"pilot_AV8B_helmet", 0 ,"Pilot_AV8B_hgu68",false};
+	{"pilot_AV8B_helmet", DECAL ,"311_Helmet_Art_Claws",false};
+	
+	{"AV8B_T1_FUSE1", 0 ,"AV8_T1_VMA311_CAG",false};
+	{"AV8B_T1_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T1_FUSE10", 0 ,"AV8_T1_VMA311_CAG",false};
+	{"AV8B_T1_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE1", 0 ,"AV8_T2_VMA311_CAG",false};
+	{"AV8B_T2_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE10", 0 ,"AV8_T2_VMA311_CAG",false};
+	{"AV8B_T2_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_FUSE1", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B_T3_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T3_FUSE10", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B_T3_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL1", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B_T3_TAIL1", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL10", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B_T3_TAIL10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL100", 0 ,"AV8_T3_VMA311_CAG",false};
+	{"AV8B_T3_TAIL100", DECAL ,"empty",true};
+	
+    {"AV8B1", ROUGHNESS_METALLIC ,"AV8_T1_RoughMet",true};
+	{"AV8B2", ROUGHNESS_METALLIC ,"AV8_T2_RoughMet",true};
+	{"AV8B3", ROUGHNESS_METALLIC ,"AV8_T3_RoughMet",true};
+	{"AV8B4", ROUGHNESS_METALLIC ,"AV8_T4_RoughMet",true};
+	{"AV8B7", ROUGHNESS_METALLIC ,"AV8_T7_RoughMet",true};
+	{"AV8B8", ROUGHNESS_METALLIC ,"AV8_T8_RoughMet",true};
+	{"pilot_AV8B_helmet", ROUGHNESS_METALLIC ,"Pilot_AV8B_hgu68_RoughMet",true};
+	{"pilot_AV8B_body", ROUGHNESS_METALLIC ,"pilot_AV8B_RoughMet",true};
+}
+
+name = "VMA-311- 01 - CAG"
+
+countries = {"USA"}
