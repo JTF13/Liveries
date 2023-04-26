03e1073cd0bdc0b5a1fe4faf1dbd81983590103d
James <jimmycmcleod@gmail.com>
2023-04-26 00:46:07 -0400

    Blacksnakes Update
    - Blacksnakes sqd skin overhaul
    
    VF-103:
    - ENS. Mitch "Needles" - 111 added to roster

---
+++ A-10CII/Blacksnakes-FortWayne163rd/description.lua
@@ -0,0 +1,95 @@
+livery = {
+	
+	{"A-10C_PAINT_1-L",	DIFFUSE			,	"a10cii_1l", false};
+	--{"A-10C_PAINT_1-L",	NORMAL_MAP			,	"a10cii_1l_normal", false};
+	{"A-10C_PAINT_1-L",	13			,	"a10cii_1l_roughmet", false};
+
+	{"A-10C_PAINT_1-g",	DIFFUSE			,	"a10cii_1g", false};
+	--{"A-10C_PAINT_1-g",	NORMAL_MAP			,	"a10cii_1g_normal", false};
+	{"A-10C_PAINT_1-g",	13			,	"a10cii_1g_roughmet", false};
+	{"A-10C_PAINT_1-e",	DIFFUSE			,	"a10cii_1e", false};
+	--{"A-10C_PAINT_1-e",	NORMAL_MAP			,	"a10cii_1e_normal", false};
+	{"A-10C_PAINT_1-e",	13			,	"a10cii_1e_roughmet", false};
+	{"A-10C_PAINT_1-j",	DIFFUSE			,	"a10cii_1j", false};
+	{"A-10C_PAINT_1-j",	NORMAL_MAP			,	"a10cii_1j_normal", false};
+	{"A-10C_PAINT_1-j",	13			,	"a10cii_1j_roughmet", false};
+
+
+	{"A-10_Ruber Main",	DIFFUSE			,	"a10cii_1j", false};
+	{"A-10_Ruber Main",	13			,	"a10cii_1j_roughmet", false};
+
+	{"A-10_Ruber F",	DIFFUSE			,	"a10cii_1f", false};
+	{"A-10_Ruber F",	13			,	"a10cii_1f_roughmet", false};
+
+
+
+
+
+
+	{"A-10C_PAINT_1-h",	DIFFUSE			,	"a10cii_1h", false};
+	--{"A-10C_PAINT_1-h",	NORMAL_MAP			,	"a10cii_1h_normal", false};
+	{"A-10C_PAINT_1-h",	13			,	"a10cii_1h_roughmet", false};
+	{"A-10C_PAINT_1-b",	DIFFUSE			,	"a10cii_1b", false};
+	--{"A-10C_PAINT_1-b",	NORMAL_MAP			,	"a10cii_1b_normal", false};
+	{"A-10C_PAINT_1-b",	13			,	"a10cii_1b_roughmet", false};
+	
+	{"A-10C_PAINT_1-c",	DIFFUSE			,	"a10cii_1c", false};
+	{"A-10C_PAINT_1-c",	NORMAL_MAP			,	"a10cii_1c_normal", false};
+	{"A-10C_PAINT_1-c",	13			,	"a10cii_1c_roughmet", false};
+	{"A-10C_PAINT_1-a",	DIFFUSE			,	"a10cii_1a", false};
+	{"A-10C_PAINT_1-a",	NORMAL_MAP			,	"a10cii_1a_normal", false};
+	{"A-10C_PAINT_1-a",	13			,	"a10cii_1a_roughmet", false};
+	--{"A-10C_PAINT_1-ad",	DIFFUSE			,	"a10cii_1ad", false};
+	--{"A-10C_PAINT_1-ad",	NORMAL_MAP			,	"a10cii_1ad_normal", false};
+	--{"A-10C_PAINT_1-ad",	13			,	"a10cii_1ad_roughmet", false};
+	{"A-10C_PAINT_1-d",	DIFFUSE			,	"a10cii_1d", false};
+	--{"A-10C_PAINT_1-d",	NORMAL_MAP			,	"a10cii_1d_normal", false};
+	{"A-10C_PAINT_1-d",	13			,	"a10cii_1d_roughmet", false};
+	{"A-10C_PAINT_1-k",	DIFFUSE			,	"a10cii_1k", false};
+	--{"A-10C_PAINT_1-k",	NORMAL_MAP			,	"a10cii_1k_normal", false};
+	{"A-10C_PAINT_1-k",	13			,	"a10cii_1k_roughmet", false};
+	{"A-10C_PAINT_1-f",	DIFFUSE			,	"a10cii_1f", false};
+	--{"A-10C_PAINT_1-f",	NORMAL_MAP			,	"a10cii_1f_normal", false};
+	{"A-10C_PAINT_1-f",	13			,	"a10cii_1f_roughmet", false};
+	{"A-10C_PAINT_1-i",	DIFFUSE			,	"a10cii_1i", false};
+	--{"A-10C_PAINT_1-i",	NORMAL_MAP			,	"a10cii_1i_normal", false};
+	{"A-10C_PAINT_1-i",	13			,	"a10cii_1i_roughmet", false};
+
+	
+
+
+
+
+
+
+	--{"pilot_F18_helmet_glass",	DIFFUSE			,	"pilot_a10_visor", false};
+	--{"pilot_F18_helmet_glass",	13			,	"pilot_a10_visor_roughmet", false};
+	--{"pilot_F18_belts",	DIFFUSE			,	"pilot_a10_belts", false};
+	--{"pilot_F18_belts",	NORMAL_MAP			,	"pilot_a10_belts_nm", false};
+	--{"pilot_F18_belts",	13			,	"pilot_a10_belts_roughmet", false};
+	{"pilot_A10_helmet",	DIFFUSE			,	"pilot_a10_helmet", false};
+	--{"pilot_A10_helmet",	NORMAL_MAP			,	"pilot_a10_helmet_nm", false};
+	--{"pilot_A10_helmet",	13			,	"pilot_a10_helmet_roughmet", false};
+
+	{"pilot_A10",	DIFFUSE			,	"pilot_a10", false};
+	--{"pilot_A10",	NORMAL_MAP			,	"pilot_a10_nm", false};
+	--{"pilot_A10",	13			,	"pilot_a10_roughmet", false};
+
+	
+	{"A-10 Eject Seat",	DIFFUSE			,	"a-10_eject_seat 1024", false};
+	
+
+	{"ALQ-184_Main",	DIFFUSE			,	"ALQ-184", false};
+	
+	{"A-10C_Cockpit",	DIFFUSE			,	"a-10c_cockpit", false};
+
+	{"A-10_Number", 0 ,"empty",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
+}
+
+
+countries = {"USA"}
+
+--lee1hy
