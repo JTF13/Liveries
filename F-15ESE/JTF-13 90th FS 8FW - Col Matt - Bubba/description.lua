bdf6ef9c76aa119ef8e771dfb3336640bac177eb
James <jimmycmcleod@gmail.com>
2023-07-15 19:07:15 -0400

    90th F-15 Strike Eagle Liveries
    James "Mouse" presents the initial iteration of our 8FW F-15E Strike Eagle liveries!

---
+++ F-15ESE/JTF-13 90th FS 8FW - Col Matt - Bubba/description.lua
@@ -0,0 +1,228 @@
+livery = {
+-- NOSE
+	{"F-15E_01",	0,	                "F-15e_E01_A_CLEAN",		true};
+	{"F-15E_01",	1,	                "F-15e_E01_NRM",	true};
+	{"F-15E_01",	ROUGHNESS_METALLIC,	"F-15e_E01_RoughMet", true};
+	{"F-15E_01",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+	
+-- -- TOP
+	-- {"F-15E_02",	0,	                "F-15e_E02_COMBAT",			true};
+	-- {"F-15E_02",	1,	                "F-15e_E02_NRM",	true};
+	-- {"F-15E_02",	ROUGHNESS_METALLIC,	"F-15e_E02_RoughMet_COMBAT", true};
+
+	-- {"F-15E_02SB",	0,	                "F-15e_E02_COMBAT",			true};
+	-- {"F-15E_02SB",	1,	                "F-15e_E02_NRM",	true};
+	-- {"F-15E_02SB",	ROUGHNESS_METALLIC,	"F-15e_E02_RoughMet_COMBAT", true};
+	
+-- -- BOTTOM
+	-- {"F-15E_03",	0,	                "F-15e_E03_COMBAT",			true};
+	-- {"F-15E_03",	1,	                "F-15e_E03_NRM",	true};
+	-- {"F-15E_03",	ROUGHNESS_METALLIC,	"F-15e_E03_RoughMet_COMBAT", true};
+	
+-- -- WING1
+	-- {"F-15E_04",	0,	                "F-15e_E04_C_COMBAT",			true};
+	-- {"F-15E_04",	1,	                "F-15e_E04_NRM",	true};
+	-- {"F-15E_04",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+-- --AILERON1	
+	-- {"F-15E_04AT",	0,	                "F-15e_E04_C_COMBAT",			true};
+	-- {"F-15E_04AT",	1,	                "F-15e_E04_NRM",	true};
+	-- {"F-15E_04AT",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+
+	-- {"F-15E_04AB",	0,	                "F-15e_E04_C_COMBAT",			true};
+	-- {"F-15E_04AB",	1,	                "F-15e_E04_NRM",	true};
+	-- {"F-15E_04AB",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+-- --FLAP1
+	-- {"F-15E_04FT",	0,	                "F-15e_E04_C_COMBAT",			true};
+	-- {"F-15E_04FT",	1,	                "F-15e_E04_NRM",	true};
+	-- {"F-15E_04FT",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+
+	-- {"F-15E_04FB",	0,	                "F-15e_E04_C_COMBAT",			true};
+	-- {"F-15E_04FB",	1,	                "F-15e_E04_NRM",	true};
+	-- {"F-15E_04FB",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+	
+-- -- WING2
+	-- {"F-15E_05",	0,	                "F-15e_E05_C_COMBAT",			true};
+	-- {"F-15E_05",	1,	                "F-15e_E05_NRM",	true};
+	-- {"F-15E_05",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+-- --ALIERON2												 
+	-- {"F-15E_05AT",	0,	                "F-15e_E05_C_COMBAT",			true};
+	-- {"F-15E_05AT",	1,	                "F-15e_E05_NRM",	true};
+	-- {"F-15E_05AT",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+	-- {"F-15E_05AB",	0,	                "F-15e_E05_C_COMBAT",			true};
+	-- {"F-15E_05AB",	1,	                "F-15e_E05_NRM",	true};
+	-- {"F-15E_05AB",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};		
+-- --FLAP2
+	-- {"F-15E_05FT",	0,	                "F-15e_E05_C_COMBAT",			true};
+	-- {"F-15E_05FT",	1,	                "F-15e_E05_NRM",	true};
+	-- {"F-15E_05FT",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+	-- {"F-15E_05FB",	0,	                "F-15e_E05_C_COMBAT",			true};
+	-- {"F-15E_05FB",	1,	                "F-15e_E05_NRM",	true};
+	-- {"F-15E_05FB",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+-- TAIL
+	{"F-15E_06",	0,	                "F-15e_E06",		true};
+	{"F-15E_06",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", true};
+	{"F-15E_06",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+-- --LEFT STAB
+	-- {"F-15E_06LS",	0,	                "F-15e_E06_A_COMBAT",	true};
+	-- {"F-15E_06LS",	1,	                "F-15e_E06_NRM",	true};
+	-- {"F-15E_06LS",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	-- {"F-15E_06LS",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+-- --RIGHT STAB	
+	-- {"F-15E_06RS",	0,	                "F-15e_E06_A_COMBAT",	true};
+	-- {"F-15E_06RS",	1,	                "F-15e_E06_NRM",	true};
+	-- {"F-15E_06RS",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	-- {"F-15E_06RS",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+-- --LEFT RUDDER	
+	-- {"F-15E_06LR",	0,	                "F-15e_E06_A_COMBAT",	true};
+	-- {"F-15E_06LR",	1,	                "F-15e_E06_NRM",	true};
+	-- {"F-15E_06LR",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	-- {"F-15E_06LR",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+-- --RIGHT RUDDER
+	-- {"F-15E_06RR",	0,	                "F-15e_E06",	true};
+	-- {"F-15E_06RR",	1,	                "F-15e_E06_NRM",	true};
+	-- {"F-15E_06RR",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", true};
+	-- {"F-15E_06RR",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+
+-- CFT
+	{"F-15E_07",	0,	                "F-15e_E07",		true};
+	{"F-15E_07",	1,	                "F-15e_E07_NRM",	true};
+	{"F-15E_07",	ROUGHNESS_METALLIC,	"F-15e_E07_RoughMet",	true};
+	{"F-15E_07",	DECAL,	    		"JTF-13 90th FS 8FW - Col Matt - Bubba",	false};
+	
+-- EXTRA
+	{"F-15E_08",	0,	                "F-15e_E08",			true};
+	{"F-15E_08",	1,	                "F-15e_E08_NRM",	true};
+	{"F-15E_08",	ROUGHNESS_METALLIC,	"F-15e_E08_RoughMet", true};
+	
+
+
+--BORT_NUMBER----------------------------------------------------------------------------------------
+
+
+
+--F-15E_REFUEL-DECAL	
+    {"F-15E_REFUEL-DECAL",	0,	                "F-15E_REFUEL_AF-88-1",				true};
+
+
+--F-15E_REFUEL-NUMBER
+    {"F-15E_REFUEL-NUMBER",	0,	                "F-15E_WHT_Numbers",				true};
+
+
+--F-15E_TAIL-DECAL
+    {"F-15E_TAIL-DECAL",	0,	                "empty",				true};
+	
+--F-15E_TAIL-DECAL-L	
+    {"F-15E_TAIL-DECAL-L",	0,	                "F-15E_E06",					true};
+	{"F-15E_TAIL-DECAL-L",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-DECAL-L",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", 			true};
+    {"F-15E_TAIL-DECAL-L",	DECAL,	            "empty",			true};
+	
+--F-15E_TAIL-DECAL-R
+    {"F-15E_TAIL-DECAL-R",	0,	                "F-15E_E06",					true};
+	{"F-15E_TAIL-DECAL-R",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-DECAL-R",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", 			true};
+    {"F-15E_TAIL-DECAL-R",	DECAL,	            "empty",			true};
+	
+--F-15E_TAIL-NUMBER                             
+    {"F-15E_TAIL-NUMBER",	0,	                "empty",			true};
+	
+--F-15E_TAIL-NUMBER-L
+	{"F-15E_TAIL-NUMBER-L",	0,	                "F-15E_E06",					true};
+	{"F-15E_TAIL-NUMBER-L",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-NUMBER-L",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", 			true};
+    {"F-15E_TAIL-NUMBER-L",	DECAL,	            "empty",			true};
+
+--F-15E_TAIL-NUMBER-R                            
+	{"F-15E_TAIL-NUMBER-R",	0,	                "F-15E_E06",					true};
+	{"F-15E_TAIL-NUMBER-R",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-NUMBER-R",	ROUGHNESS_METALLIC,	"F-15e_E06_RoughMet", 			true};
+    {"F-15E_TAIL-NUMBER-R",	DECAL,	            "empty",			true};
+	
+--PILOT
+	{"F-15EC_05",	0,	                "F-15E_EC05B2",					true};
+	{"F-15EC_05",	1,	                "F-15E_EC05_NRM",				true};
+	{"F-15EC_05",	ROUGHNESS_METALLIC,	"F-15E_EC05_RoughMet", 			true};
+
+	{"F-15EC_06",	0,	                "F-15E_EC06B2",					true};
+	{"F-15EC_06",	1,	                "F-15E_EC06_NRM",				true};
+	{"F-15EC_06",	ROUGHNESS_METALLIC,	"F-15E_EC06_RoughMet", 			true};
+
+	{"F-15EC_07",	0,	                "F-15E_EC07A1",					true};
+	{"F-15EC_07",	1,	                "F-15E_EC07A_NRM",				true};
+	{"F-15EC_07",	ROUGHNESS_METALLIC,	"F-15E_EC07A_RoughMet", 		true};
+--    {"F-15EC_07",	DECAL,	            "F-15EC_07_Decal",			true};
+
+--WSO
+	{"F-15EC_05R",	0,	                "F-15E_EC05B1",					true};
+	{"F-15EC_05R",	1,	                "F-15E_EC05_NRM",				true};
+	{"F-15EC_05R",	ROUGHNESS_METALLIC,	"F-15E_EC05_RoughMet", 			true};
+
+	{"F-15EC_06R",	0,	                "F-15E_EC06B1",					true};
+	{"F-15EC_06R",	1,	                "F-15E_EC06_NRM",				true};
+	{"F-15EC_06R",	ROUGHNESS_METALLIC,	"F-15E_EC06_RoughMet", 			true};
+
+	{"F-15EC_07R",	0,	                "F-15E_EC07A1",					true};
+	{"F-15EC_07R",	1,	                "F-15E_EC07A_NRM",				true};
+	{"F-15EC_07R",	ROUGHNESS_METALLIC,	"F-15E_EC07A_RoughMet", 			true};
+--    {"F-15EC_07R",	DECAL,	            "F-15EC_07_Decal",			true};
+
+
+-----------------------------VR/OTHER PILOTS-----------------------------------------
+
+--PILOT
+	{"F-15E_VR-Pilot1",	0,	                "F-15E_TP1B2",					true};
+	{"F-15E_VR-Pilot1",	1,	                "F-15E_TP1_NRM",				true};
+	{"F-15E_VR-Pilot1",	ROUGHNESS_METALLIC,	"F-15E_TP1_RoughMet", 			true};
+
+	{"F-15E_VR-Pilot2",	0,	                "F-15E_TP2B2",					true};
+	{"F-15E_VR-Pilot2",	1,	                "F-15E_TP2_NRM",				true};
+	{"F-15E_VR-Pilot2",	ROUGHNESS_METALLIC,	"F-15E_TP2_RoughMet", 			true};
+
+	{"F-15E_VR-Pilot3",	0,	                "F-15E_TP3B",					true};
+	{"F-15E_VR-Pilot3",	1,	                "F-15E_TP3_NRM",				true};
+	{"F-15E_VR-Pilot3",	ROUGHNESS_METALLIC,	"F-15E_TP3_RoughMet", 			true};
+
+--WSO
+	{"F-15E_VR-Pilot1R",	0,	                "F-15E_TP1B1",				true};
+	{"F-15E_VR-Pilot1R",	1,	                "F-15E_TP1_NRM",			true};
+	{"F-15E_VR-Pilot1R",	ROUGHNESS_METALLIC,	"F-15E_TP1_RoughMet", 		true};
+
+	{"F-15E_VR-Pilot2R",	0,	                "F-15E_TP2B1",				true};
+	{"F-15E_VR-Pilot2R",	1,	                "F-15E_TP2_NRM",			true};
+	{"F-15E_VR-Pilot2R",	ROUGHNESS_METALLIC,	"F-15E_TP2_RoughMet", 		true};
+
+	{"F-15E_VR-Pilot3R",	0,	                "F-15E_TP3B",				true};
+	{"F-15E_VR-Pilot3R",	1,	                "F-15E_TP3_NRM",			true};
+	{"F-15E_VR-Pilot3R",	ROUGHNESS_METALLIC,	"F-15E_TP3_RoughMet", 		true};
+
+--Visor
+	{"F-15EC_07_Visor",	0,	                "F-15E_EC07A1",					true};
+	{"F-15EC_07_Visor",	1,	                "F-15E_EC07A_NRM",				true};
+	{"F-15EC_07_Visor",	ROUGHNESS_METALLIC,	"F-15E_EC07A_RoughMet", 		true};
+
+	{"F-15EC_07R_Visor",	0,	                "F-15E_EC07A1",					true};
+	{"F-15EC_07R_Visor",	1,	                "F-15E_EC07A_NRM",				true};
+	{"F-15EC_07R_Visor",	ROUGHNESS_METALLIC,	"F-15E_EC07A_RoughMet", 			true};
+}
+
+
+
+
+name = "JTF-13 90th FS 8FW - Col Matt - Bubba"  --1674
+countries = {"USA"}
+
+custom_args = 
+{
+[32]   = 0.4, -- 1
+[31]   = 0.7, -- 10
+[442]  = 0.6, -- 100 
+[1000] = 0.0, -- change of type of board number (0.0 - No Numbers, 0.1 - 3 Numbers, 0.2 - 4 Numbers starts with "1", 0.3 - Full 4 Numbers)
+[1001] = 0.1, -- vis refuel board number (0.0 - No Numbers, 0.1 Numbers, 0.2 - Numbers Early)
+[1002] = 0.0, -- change of type intake board number 
+[1003] = 0.12, -- vis Nose Gear board number (0.0 - 4 Numbers 0.0 to 0.05 adjusts the placement, 0.1 - 5 Numbers)
+}
+
