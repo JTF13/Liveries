cb149a1230764478c0c0a5c642d3e8114f60caec
JamesL <james.le5871@gmail.com>
2023-10-16 14:15:41 -0700

    F-15E Aggressor Skins for RIMPAC

---
+++ F-15ESE/JTF13 90FS AGGRESSOR-SplinterBlue/description.lua
@@ -0,0 +1,171 @@
+livery = {
+-- NOSE
+	{"F-15E_01",	0,	                "E01",		false};
+	{"F-15E_01",	1,	                "F-15e_E01_NRM",	true};
+	{"F-15E_01",	ROUGHNESS_METALLIC,	"F-15e_E01_RoughMet_COMBAT", true};
+	{"F-15E_01",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+	
+-- TOP
+	{"F-15E_02",	0,	                "E02",			false};
+	{"F-15E_02",	1,	                "F-15e_E02_NRM",	true};
+	{"F-15E_02",	ROUGHNESS_METALLIC,	"F-15e_E02_RoughMet_COMBAT", true};
+
+	{"F-15E_02SB",	0,	                "E02",			false};
+	{"F-15E_02SB",	1,	                "F-15e_E02_NRM",	true};
+	{"F-15E_02SB",	ROUGHNESS_METALLIC,	"F-15e_E02_RoughMet_COMBAT", true};
+	
+-- BOTTOM
+	{"F-15E_03",	0,	                "E03",			false};
+	{"F-15E_03",	1,	                "F-15e_E03_NRM",	true};
+	{"F-15E_03",	ROUGHNESS_METALLIC,	"F-15e_E03_RoughMet_COMBAT", true};
+	
+-- WING1
+	{"F-15E_04",	0,	                "E04",			false};
+	{"F-15E_04",	1,	                "F-15e_E04_NRM",	true};
+	{"F-15E_04",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+--AILERON1	
+	{"F-15E_04AT",	0,	                "E04",			false};
+	{"F-15E_04AT",	1,	                "F-15e_E04_NRM",	true};
+	{"F-15E_04AT",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+
+	{"F-15E_04AB",	0,	                "E04",			false};
+	{"F-15E_04AB",	1,	                "F-15e_E04_NRM",	true};
+	{"F-15E_04AB",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+--FLAP1
+	{"F-15E_04FT",	0,	                "E04",			false};
+	{"F-15E_04FT",	1,	                "F-15e_E04_NRM",	true};
+	{"F-15E_04FT",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+
+	{"F-15E_04FB",	0,	                "E04",			false};
+	{"F-15E_04FB",	1,	                "F-15e_E04_NRM",	true};
+	{"F-15E_04FB",	ROUGHNESS_METALLIC,	"F-15e_E04_C_RoughMet_COMBAT", true};
+	
+-- WING2
+	{"F-15E_05",	0,	                "E05",			false};
+	{"F-15E_05",	1,	                "F-15e_E05_NRM",	true};
+	{"F-15E_05",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+--ALIERON2												 
+	{"F-15E_05AT",	0,	                "E05",			false};
+	{"F-15E_05AT",	1,	                "F-15e_E05_NRM",	true};
+	{"F-15E_05AT",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+	{"F-15E_05AB",	0,	                "E05",			false};
+	{"F-15E_05AB",	1,	                "F-15e_E05_NRM",	true};
+	{"F-15E_05AB",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};		
+--FLAP2
+	{"F-15E_05FT",	0,	                "E05",			false};
+	{"F-15E_05FT",	1,	                "F-15e_E05_NRM",	true};
+	{"F-15E_05FT",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+	{"F-15E_05FB",	0,	                "E05",			false};
+	{"F-15E_05FB",	1,	                "F-15e_E05_NRM",	true};
+	{"F-15E_05FB",	ROUGHNESS_METALLIC,	"F-15e_E05_C_RoughMet_COMBAT", true};
+
+-- TAIL
+	{"F-15E_06",	0,	                "E06",		false};
+	{"F-15E_06",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	{"F-15E_06",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+--LEFT STAB
+	{"F-15E_06LS",	0,	                "E06",	false};
+	{"F-15E_06LS",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06LS",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	{"F-15E_06LS",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+--RIGHT STAB	
+	{"F-15E_06RS",	0,	                "E06",	false};
+	{"F-15E_06RS",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06RS",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	{"F-15E_06RS",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+--LEFT RUDDER	
+	{"F-15E_06LR",	0,	                "E06",	false};
+	{"F-15E_06LR",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06LR",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	{"F-15E_06LR",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+--RIGHT RUDDER
+	{"F-15E_06RR",	0,	                "E06",	false};
+	{"F-15E_06RR",	1,	                "F-15e_E06_NRM",	true};
+	{"F-15E_06RR",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", true};
+	{"F-15E_06RR",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+
+-- CFT
+	{"F-15E_07",	0,	                "E07",		false};
+	{"F-15E_07",	1,	                "F-15e_E07_NRM",	true};
+	{"F-15E_07",	ROUGHNESS_METALLIC,	"F-15e_E07_RoughMet_COMBAT",	true};
+	{"F-15E_07",	DECAL,	    		"JTF-13 90th FS PairODiceAGGR",	false};
+	
+-- EXTRA
+	{"F-15E_08",	0,	                "E08",			false};
+	{"F-15E_08",	1,	                "F-15e_E08_NRM",	true};
+	{"F-15E_08",	ROUGHNESS_METALLIC,	"F-15e_E08_RoughMet_COMBAT", true};
+
+-- DROP TANKS
+	{"F-15E_13",	0,	                "E13",			false};
+	{"F-15E_13",	1,	                "F-15E_E13_NRM",	true};
+	{"F-15E_13",	ROUGHNESS_METALLIC,	"F-15E_E13_RoughMet", true};
+	
+-- PODS/PYLONS
+	{"F-15E_14",	0,	                "E14",			false};
+	{"F-15E_14",	1,	                "F-15E_E14_NRM",	true};
+	{"F-15E_14",	ROUGHNESS_METALLIC,	"F-15E_E14_RoughMet", true};
+	
+
+
+--BORT_NUMBER----------------------------------------------------------------------------------------
+
+
+
+--F-15E_REFUEL-DECAL	
+    {"F-15E_REFUEL-DECAL",	0,	                "F-15E_REFUEL_AF-91-0",				true};
+
+
+--F-15E_REFUEL-NUMBER
+    {"F-15E_REFUEL-NUMBER",	0,	                "F-15E_WHT_Numbers",				true};
+
+
+--F-15E_TAIL-DECAL
+    {"F-15E_TAIL-DECAL",	0,	                "F-15E_TAIL_AF-91-0",				true};
+	
+--F-15E_TAIL-DECAL-L	
+    {"F-15E_TAIL-DECAL-L",	0,	                "E06",					false};
+	{"F-15E_TAIL-DECAL-L",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-DECAL-L",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", 			true};
+    {"F-15E_TAIL-DECAL-L",	DECAL,	            "F-15E_TAIL_AF-91-0-L-Shadow",			false};
+	
+--F-15E_TAIL-DECAL-R
+    {"F-15E_TAIL-DECAL-R",	0,	                "E06",					false};
+	{"F-15E_TAIL-DECAL-R",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-DECAL-R",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", 			true};
+    {"F-15E_TAIL-DECAL-R",	DECAL,	            "F-15E_TAIL_AF-91-0-R-Shadow",			false};
+	
+--F-15E_TAIL-NUMBER                             
+    {"F-15E_TAIL-NUMBER",	0,	                "F-15E_BLK_Numbers",			true};
+	
+--F-15E_TAIL-NUMBER-L
+	{"F-15E_TAIL-NUMBER-L",	0,	                "E06",					false};
+	{"F-15E_TAIL-NUMBER-L",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-NUMBER-L",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", 			true};
+    {"F-15E_TAIL-NUMBER-L",	DECAL,	            "F-15E_BLK_L-Shadows_Numbers",			false};
+
+--F-15E_TAIL-NUMBER-R                            
+	{"F-15E_TAIL-NUMBER-R",	0,	                "E06",					false};
+	{"F-15E_TAIL-NUMBER-R",	1,	                "F-15e_E06_NRM",				true};
+	{"F-15E_TAIL-NUMBER-R",	ROUGHNESS_METALLIC,	"F-15e_E06_A_RoughMet_COMBAT", 			true};
+    {"F-15E_TAIL-NUMBER-R",	DECAL,	            "F-15E_BLK_R-Shadows_Numbers",			false};
+}
+
+
+
+name = "JTF13 90FS AGGRESSOR - Splinter Blue" --293
+countries = {"USA"}
+
+custom_args = 
+{
+[32]   = 0.3, -- 1
+[31]   = 0.9, -- 10
+[442]  = 0.2, -- 100 
+[1000] = 0.1, -- change of type of board number (0.0 - No Numbers, 0.1 - 3 Numbers, 0.2 - 4 Numbers starts with "1", 0.3 - Full 4 Numbers)
+[1001] = 0.1, -- vis refuel board number (0.0 - No Numbers, 0.1 Numbers, 0.2 - Numbers Early)
+[1002] = 0.0, -- change of type intake board number 
+[1003] = 0.02, -- vis Nose Gear board number (0.0 - 4 Numbers 0.0 to 0.05 adjusts the placement, 0.1 - 5 Numbers)
+}
+
