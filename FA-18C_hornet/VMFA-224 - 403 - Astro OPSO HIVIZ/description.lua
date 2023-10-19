764bf39293d0729ec2ab83b483aeb02f594e4d7d
Stlshady <90146008+Stlshady@users.noreply.github.com>
2023-10-19 15:30:14 -0500

    New helmet art for Astroturf
    Heat- Seekin' Puerto Rican

--- FA-18C_hornet/VMFA-224 - 403 - Astro OPSO HIVIZ/description.lua
+++ FA-18C_hornet/VMFA-224 - 403 - Astro OPSO HIVIZ/description.lua
@@ -7,7 +7,7 @@ livery = {
 	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
 	
 	--Pilot Maps
-	{"pilot_F18_helmet", 0, "../VMFA-224 - 000 - Line/pilot_F18_helmet2", false};
+	{"pilot_F18_helmet", 0,"pilot_F18_helmet_astro", false};
 	{"pilot_F18_patch", 0 ,"../VMFA-224 - 000 - Line/Hornet_Pilot_Patch",false};
 	{"pilot_F18", 0 ,"../VMFA-224 - 000 - Line/pilot_F18",false};
 	{"pilot_F18", 2 ,"pilot_F18_roughmet",true};
