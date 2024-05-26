f01b1645ab92b1ca40810743dd00dcdcdc8befba
James <jimmycmcleod@gmail.com>
2024-05-26 13:25:03 -0400

    Egyptian Air Force Skins
    EAF skins for the upcoming campaign.
    - F-16
    - M-2000
    - E-2D
    - MiG-29S
    
    - Path naming fix for E-2

---
+++ E-2C/Egyptian Air Force/description.lua
@@ -0,0 +1,17 @@
+livery = {
+	{"e2d_fus", 0 ,"e2d_fus_eaf",false};
+	{"e2d_fus", ROUGHNESS_METALLIC ,"e2d_fus_RoughMet_eaf",false};
+	{"e2d_wing", 0 ,"e2d_wing_eaf",false};
+	{"e2d_wing", ROUGHNESS_METALLIC ,"e2d_wing_RoughMet_eaf",false};
+	
+	{"e2d_pilot", 0, "pilot_e2_eaf", false};
+	{"e2d_wso", 0, "wso_e2_eaf", false};
+	{"e2d_pilot_helmet", 0, "hgu55_eaf.dds", false};
+
+	{"f15numbers", 0, "empty", true};
+
+}
+	
+name = "Egyptian Air Force"
+
+--countries = {"USA","JPN",}
