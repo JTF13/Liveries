dc1bcd272f3f85ef5a384603365d2dce164df3e8
James <jimmycmcleod@gmail.com>
2024-06-08 11:16:24 -0400

    Egyptian C-130
    - EAF C-130 Hercules skin added

---
+++ C-130/Egyptian Air Force C-130 Hercules/description.lua
@@ -0,0 +1,21 @@
+--This livery is supplied as freeware and is subject to copyright.
+
+--You may use this livery for your own personal or group use.
+
+-- You may NOT copy, change or modify any of the texture files in any way or publish elsewhere without permission of the original artist.
+
+--Any breach of these conditions will be in violation of the original artists Intellectual Property Rights and will be subject appropriate legal proceedings, penalties and costs as awarded.
+
+
+
+livery = {
+	{"C-130_fusel", 0 ,"EAF_C-130_map_fus",false};
+	{"C-130_wing", 0 ,"EAF_C-130_map_wings",false};
+	{"C-130_wing_2", 0 ,"EAF_C-130_map_wings_2",false};
+	{"C-130_notes", 0 ,"C-130_notes_empty",true};
+	{"pilots_C-130", 0 ,"pilots_c-130",true}; 
+ 
+	
+}
+
+name = "Egyptian Air Force C-130 Hercules by 'ballooningrich'"
