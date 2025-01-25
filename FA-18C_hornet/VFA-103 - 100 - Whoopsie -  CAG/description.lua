e8a5a8c487d871b2fa28feaa3b35c98e117ca531
StlShady <christophershader@mac.com>
2025-01-24 23:45:41 -0600

    2x More Hornets
    001- Wizard JTF-13 Commander livery
    100- VFA-103 Jolly Rogers Hornet - Whoopsie CAG

---
+++ FA-18C_hornet/VFA-103 - 100 - Whoopsie -  CAG/description.lua
@@ -0,0 +1,99 @@
+livery = {
+
+	{"f18c1", 0 ,"F18C_1_DIFF",false};
+	{"f18c1", 1 ,"F18C_1_DIFF_nm",false};
+	{"f18c1", ROUGHNESS_METALLIC ,"F18C_1_DIFF_RoughMet",false};
+	
+	{"f18c2", 0 ,"F18C_2_DIFF",false};
+	{"f18c2", 1 ,"F18C_2_DIFF_nm",false};
+	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIFF_RoughMet",false};
+
+	{"FPU_8A", 0 ,"FPU_8A",false};
+	{"FPU_8A", ROUGHNESS_METALLIC ,"FPU_8A_RoughMet",false};
+	
+	{"f18c_wheel_101", 0, "f18c_wheel", false};
+	{"f18c_wheel_102", 0, "f18c_wheel", false};
+	{"f18c_wheel_103", 0, "f18c_wheel", false};
+	
+	{"f18c1_number_nose_right", 0 ,"F18C_1_DIFF",false};
+	{"f18c1_number_nose_right", ROUGHNESS_METALLIC ,"F18C_1_DIFF_RoughMet",false};
+	{"f18c1_number_nose_right", DECAL ,"empty", true};;
+	
+	{"f18c1_number_nose_left", 0 ,"F18C_1_DIFF",false};
+	{"f18c1_number_nose_left", ROUGHNESS_METALLIC ,"F18C_1_DIFF_RoughMet",false};
+	{"f18c1_number_nose_left", DECAL ,"empty", true};	
+
+	{"f18c2_kil_right", 0 ,"F18C_2_DIFF",false};
+	{"f18c2_kil_right", ROUGHNESS_METALLIC ,"F18C_2_DIFF_RoughMet",false};
+	{"f18c2_kil_right", DECAL ,"empty", true};
+	
+	{"f18c2_kil_left", 0 ,"F18C_2_DIFF",false};
+	{"f18c2_kil_left", ROUGHNESS_METALLIC ,"F18C_2_DIFF_RoughMet",false};
+	{"f18c2_kil_left", DECAL ,"empty", true};
+	
+	{"f18c1_number_F", 0 ,"F18C_2_DIFF",false};
+	{"f18c1_number_F", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",false};
+	{"f18c1_number_F", DECAL ,"empty", true};
+
+	{"f18c2_number_X", 0 ,"F18C_2_DIFF",false};
+	{"f18c2_number_X", ROUGHNESS_METALLIC ,"F18C_2_DIFF_RoughMet",false};
+	{"f18c2_number_X", DECAL ,"empty", true};	
+	
+  	{"pilot_F18_patch", 0 ,"Pilot_F18_patch",false};
+	{"pilot_F18_patch", 1 ,"Pilot_F18_patch_nm",false};
+	{"pilot_F18_helmet_hmd", DIFFUSE, "pilot_f18_helmet_hmd", false};
+	{"pilot_F18_helmet_details", DIFFUSE, "pilot_f18_helmet_details", false};
+	{"pilot_F18_helmet", DIFFUSE, "pilot_f18_helmet", false};
+	
+	--Grey Naval bombs
+	{"GBU_12",	DIFFUSE			,	"gbu_12t_gray_diff", true};
+	{"GBU_12",	NORMAL_MAP			,	"gbu_12_nm", true};
+	{"GBU_12",	ROUGHNESS_METALLIC			,	"gbu_12_diff_roughmet", true};
+	{"MK_82", DIFFUSE, "mk_82t_grey_diff", true};
+	{"MK_82", NORMAL_MAP, "mk_82_nm", true};
+	{"MK_82", ROUGHNESS_METALLIC, "mk_82_diff_roughmet", true};
+	{"mk_83", DIFFUSE, "mk_83tgrey_diff", true};
+	{"mk_83", NORMAL_MAP, "mk_83_nm", true};
+	{"mk_83", ROUGHNESS_METALLIC, "mk_83_diff_roughmet", true};
+	{"GBU_38", DIFFUSE, "gbu_38t_diff", true};
+	{"GBU_38", NORMAL_MAP, "gbu_38_nm", true};
+	{"GBU_38", ROUGHNESS_METALLIC, "gbu_38_diff_roughmet", true};
+	{"GBU_31", DIFFUSE, "gbu_31t_gray_diff", true};
+	{"GBU_31", NORMAL_MAP, "gbu_31_nm", true};
+	{"GBU_31", ROUGHNESS_METALLIC, "gbu_31_diff_roughmet", true};
+	{"mk_84", NORMAL_MAP, "mk_84_nm", true};
+	{"mk_84", ROUGHNESS_METALLIC, "mk_84_diff_roughmet", true};
+	{"GBU_10", DIFFUSE, "gbu_10_diff", true};
+	{"GBU_10", NORMAL_MAP, "gbu_10_nm", true};
+	{"GBU_10", ROUGHNESS_METALLIC, "gbu_10_diff_roughmet", true};
+	{"GBU_16", DIFFUSE, "gbu_16t_gray_diff", true};
+	{"GBU_16", NORMAL_MAP, "gbu_16t_nm", true};
+	{"GBU_16", ROUGHNESS_METALLIC, "gbu_16t_gray_diff_roughmet", true};
+}
+name = "VFA-103 Jolly Rogers Hornet- CAG"
+countries = {"USA",}
+
+
+
+
+
+custom_args = 
+{
+ 
+[27] = 1.0, -- Tail      change of type of board number (0.0 -default USA, 0.1- )
+[1000] = 1.0, -- Flaps
+[1001] = 1.0, -- Nose
+[1002] = 1, -- Kuwait Squadron 
+[1003] = 1, -- Australian Squadron 
+[1004] = 1, -- Finland Squadron
+[1005] = 1, -- Switzerland Squadron
+[1006] = 1, -- Blue Angels Jet Team
+
+
+}
+
+
+
+
+
+	
