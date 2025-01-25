b9c25d264d2e04ae9f3efdde181ae79c98330eb8
James <jimmycmcleod@gmail.com>
2025-01-25 15:32:31 -0500

    Hornet Quick Fix
    Fix to Mayo's Tribute skin

---
+++ E-2C/VAW 125/description.lua
@@ -0,0 +1,16 @@
+livery = {
+	{"e2d_fus", 0 ,"e2d_fus",false};
+	{"e2d_fus", ROUGHNESS_METALLIC ,"e2d_fus_RoughMet",false};
+	{"e2d_wing", 0 ,"e2d_wing",false};
+	{"e2d_wing", ROUGHNESS_METALLIC ,"e2d_wing_RoughMet",false};
+	
+	{"e2d_pilot", 0, "pilot_e2_vaw", false};
+	{"e2d_wso", 0, "wso_e2_vaw", false};
+	{"e2d_pilot_helmet", 0, "hgu55_jasdf.dds", false};
+
+	{"f15numbers", 0, "empty", true};
+
+}
+
+name = "VAW-125"
+countries = {"USA"}
