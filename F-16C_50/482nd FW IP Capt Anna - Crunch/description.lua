237adeabf0a929cd9dafbb3df21a2220084b6da3
Vishari <jimmycmcleod@gmail.com>
2026-06-01 19:22:24 -0400

    Makos Update
    - Crunch Skin

---
+++ F-16C_50/482nd FW IP Capt Anna - Crunch/description.lua
@@ -0,0 +1,152 @@
+livery = {
+
+
+
+--F16_bl50_Main_1
+    {"F16_bl50_Main_1", DIFFUSE, "f16_bl50_main_1", false};
+	{"F16_bl50_Main_1", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_1_normal", false};
+	{"F16_bl50_Main_1",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_main_1_roughmet", false};
+  
+
+--F16_bl50_Main_2	
+	{"F16_bl50_Main_2", DIFFUSE, "../482nd FW IP Textures/f16_bl50_main_2", false};
+	{"F16_bl50_Main_2", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_2_normal", false};
+	{"F16_bl50_Main_2",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_main_2_roughmet", false};
+
+--F16_bl50_Main_3	
+	{"F16_bl50_Main_3", DIFFUSE, "../482nd FW IP Textures/f16_bl50_main_3", false};
+	{"F16_bl50_Main_3", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_3_normal", false};
+   	{"F16_bl50_Main_3",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_main_3_roughmet", false};
+	
+--F16_bl50_Kil	
+	{"F16_bl50_Kil", DIFFUSE, "../482nd FW IP Textures/f16_bl50_kil", false};
+	{"F16_bl50_Kil", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_kil_normal", false};
+	{"F16_bl50_Kil",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_kil_roughmet", false};
+
+
+--F16_bl50_wing_L
+    	{"F16_bl50_wing_L", DIFFUSE, "../482nd FW IP Textures/f16_bl50_wing_l", false};
+	{"F16_bl50_wing_L", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_wing_l_normal", false};
+   	{"F16_bl50_wing_L",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_wing_l_roughmet", false};
+
+--F16_bl50_wing_R	
+	{"F16_bl50_wing_R", DIFFUSE, "../482nd FW IP Textures/f16_bl50_wing_r", false};
+	{"F16_bl50_wing_R", 1, "../JTF-13 93rd FS - Makos Textures/f16_bl50_wing_r_normal", false};
+  	{"F16_bl50_wing_R",	ROUGHNESS_METALLIC,	"../482nd FW IP Textures/f16_bl50_wing_r_roughmet", false};
+
+
+
+--motor
+	{"F16_bl50_Engine", 0, "../JTF-13 93rd FS - Makos Textures/f16_bl50_engine", false};
+	--{"F16_bl50_Engine", 1, "f16_bl50_engine_normal", false};
+	{"F16_bl50_Engine", ROUGHNESS_METALLIC ,"../JTF-13 93rd FS - Makos Textures/F16_bl50_Engine_RoughMet",false};
+
+
+--Pilot
+	{"Pilot_F16_Suit", DIFFUSE, "pilot_f16_suit", true};
+    {"Pilot_F16_Suit", NORMAL_MAP, "pilot_f16_suit_normal", true};
+    {"Pilot_F16_Suit", ROUGHNESS_METALLIC, "pilot_f16_suit_roughmet", true};
+    
+    {"pilot_F16_hmd_details", DIFFUSE, "pilot_f16_helmet_details", false};
+    {"pilot_F16_hmd_details", NORMAL_MAP, "pilot_f16_helmet_details_normal", true};
+    {"pilot_F16_hmd_details", ROUGHNESS_METALLIC, "pilot_f16_helmet_details_roughmet", true};
+        
+    {"pilot_F16_helmet", DIFFUSE, "Pilot_f16_Helmet_Gray_Ghost", false};
+    {"pilot_F16_helmet", NORMAL_MAP, "pilot_f16_helmet_gray_normal", true};
+    {"pilot_F16_helmet", ROUGHNESS_METALLIC, "pilot_f16_helmet_gray_roughmet", true};
+    
+    {"pilot_F16_helmet_HMD",  DIFFUSE, "pilot_f16_helmet_hmd", false};
+    {"pilot_F16_helmet_HMD",  NORMAL_MAP, "pilot_f16_helmet_hmd_normal", true};
+    {"pilot_F16_helmet_HMD",  ROUGHNESS_METALLIC, "pilot_f16_helmet_hmd_roughmet", true};
+
+    {"pilot_F16_visor_glass", 0, "Pilot_F16_Helmet_Visor_Glass", false};
+    {"pilot_F16_visor_glass", 1, "", true};
+    {"pilot_F16_visor_glass", ROUGHNESS_METALLIC, "", true};
+	
+	{"pilot_F16_patch", 0 ,"../JTF-13 93rd FS - Makos Textures/pilot_F16_patch",false};
+	{"pilot_F16_patch", ROUGHNESS_METALLIC ,"../JTF-13 93rd FS - Makos Textures/pilot_F16_patch_roughmet",false};
+	{"pilot_F16_patch", 1 ,"../JTF-13 93rd FS - Makos Textures/pilot_F16_patch_normal",false};
+
+-- F16_bl50_Fuel_Tank_300Gal
+	{"Fuel_Tank_300Gal",	DIFFUSE			,	"../482nd FW IP Textures/fuel_tank_300gal", false};
+
+-- F16_bl50_Fuel_Tank_370Gal
+	{"F_16_Tank_370",	DIFFUSE			,	"../482nd FW IP Textures/fuel_tank_370gal_diff", false};
+
+--Pilones
+	{"F16_bl50_Wing_Pylon_1", 0 ,"../JTF-13 93rd FS - Makos Textures/F16_bl50_Wing_Pylon_1",false};
+	{"F16_bl50_Wing_Pylon_2", 0 ,"../JTF-13 93rd FS - Makos Textures/F16_bl50_Wing_Pylon_2",false};
+	
+	{"LAU_129", 0 ,"../JTF-13 93rd FS - Makos Textures/LAU_129_diff",false};
+--POD
+	{"F16_HTC_POD", 0 ,"../JTF-13 93rd FS - Makos Textures/F16_HTC_POD",false};
+	{"F16_HTC_POD", 13 ,"../JTF-13 93rd FS - Makos Textures/F16_HTC_POD_roughmet",false};
+	
+--AAQ28
+	{"AAQ_28", DIFFUSE, "../AAQ28/Makos2AAQ_28_diff", false};
+	{"AAQ_28", ROUGHNESS_METALLIC, "../AAQ28/aaq_28_diff_roughmet", false};
+	{"AAQ_28", NORMAL_MAP, "../AAQ28/aaq_28_nm", true};	
+	{"AAQ_28_Glass", 0, "../AAQ28/aaq_28_glass", false};
+	{"AAQ_28_Glass", ROUGHNESS_METALLIC, "../AAQ28/aaq_28_glass_roughmet", false};
+
+--BORT_NUMBER----------------------------------------------------------------------------------------
+
+
+
+--F16_bl50_FIN_DECAL
+	{"F16_bl50_FIN_DECAL",	0,	                "f16_bl50_kil",				false};
+	{"F16_bl50_FIN_DECAL",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_kil_normal",		false};
+	{"F16_bl50_FIN_DECAL",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet", false};
+	{"F16_bl50_FIN_DECAL",            DECAL,	            "F16_Caza_Decal", false};
+
+--F16_bl50_FIN_BORT_NUMBER
+	{"F16_bl50_FIN_BORT_NUMBER",	0,	                "f16_bl50_kil",				false};
+	{"F16_bl50_FIN_BORT_NUMBER",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_kil_normal",		false};
+	{"F16_bl50_FIN_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet", false};
+	{"F16_bl50_FIN_BORT_NUMBER",	DECAL,	                  "F16_bort_number_w",	false};
+
+
+--F16_bl50_NOUSE_DECAL
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	0,	                "f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	ROUGHNESS_METALLIC,	"f16_bl50_main_1_roughmet, 	false"};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	DECAL,	            "F16_bort_number", false};
+
+--F16_bl50_REFUEL_DECAL
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	0,	                "f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	ROUGHNESS_METALLIC,	"f16_bl50_main_1_roughmet",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	DECAL,	            "F16_AF_87_Decal", false};
+
+--F16_bl50_NOUSE AND REFUEL BORT_NUMBER
+	{"F16_bl50_MAT1_BORT_NUMBER",	0,	                "f16_bl50_main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_1_normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	DECAL,	            "F16_bort_number", false};
+
+--F16_bl50_INTAKE_BORT_NUMBER
+	{"F16_bl50_INTAKE_BORT_NUMBER",	0,	                "f16_bl50_main_3",				false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	1,	                "../JTF-13 93rd FS - Makos Textures/f16_bl50_main_3_normal",		false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_main_3_roughmet", false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	DECAL,	           "F16_bort_number", false};
+
+
+
+}
+name = "482nd FW IP Capt Anna - Crunch"
+--Original Files "Flash", Enhanced by MaximusRZ Oct, 2023
+--Edited By Bryan "Flash" June/2026
+countries = {}
+
+custom_args = 
+{
+[1000] = 1.0, -- change of type of board number (0.0 -default USA, 0.1- )
+[1001] = 1.0, -- vis refuel board number 
+[1002] = 1.0, -- change of type intake board number 
+[1003] = 1.0, -- vis nouse board number
+[441] = 0.2,
+[31] = 0.90,
+[32] = 0.0,
+}
+
+-- change of type intake board number
