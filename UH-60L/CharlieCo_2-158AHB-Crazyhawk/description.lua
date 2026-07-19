2b5d3f2c38140e5e8d98885593bb1b088e39f0cb
Cactus8593 <64085492+Cactus8593@users.noreply.github.com>
2026-07-19 15:04:48 -0500

    Adding the 1-52 GSAB livery for CH-47
    Added touch to have the Arctic Dragons portraited - this is for the Sugar Bears Callsign "URSA"

---
+++ UH-60L/CharlieCo_2-158AHB-Crazyhawk/description.lua
@@ -0,0 +1,44 @@
+livery = {
+	{"BH_Base",DIFFUSE,					"uh60l_base",					true};
+	{"BH_Base",ROUGHNESS_METALLIC, 		"uh60l_base_RoughMet",			true};
+	{"BH_Base",1,						"uh60l_base_Normal",			true};
+
+	{"BH_Fuselage1",DIFFUSE,			"uh60l_fuselage_01", 			false};
+	{"BH_Fuselage1",ROUGHNESS_METALLIC, "uh60l_fuselage_01_RoughMet",	false};
+	{"BH_Fuselage1",1,					"uh60l_fuselage_01_Normal",		true};
+
+	{"BH_Fuselage2",DIFFUSE,			"uh60l_fuselage_02", 			true};
+	{"BH_Fuselage2",ROUGHNESS_METALLIC, "uh60l_fuselage_02_RoughMet",	true};
+	{"BH_Fuselage2",1,					"uh60l_fuselage_02_Normal",		true};
+
+	{"BH_Fuselage3",DIFFUSE,			"uh60l_fuselage_03", 			true};
+	{"BH_Fuselage3",ROUGHNESS_METALLIC, "uh60l_fuselage_03_RoughMet",	true};
+	{"BH_Fuselage3",1,					"uh60l_fuselage_03_Normal",		true};
+
+	{"BH_Fuselage4",DIFFUSE,			"uh60l_fuselage_04", 			true};
+	{"BH_Fuselage4",ROUGHNESS_METALLIC, "uh60l_fuselage_04_RoughMet",	true};
+	{"BH_Fuselage4",1,					"uh60l_fuselage_04_Normal",		true};
+
+	{"BH_Fuselage5",DIFFUSE,			"uh60l_fuselage_05", 			true};
+	{"BH_Fuselage5",ROUGHNESS_METALLIC, "uh60l_fuselage_05_RoughMet",	true};
+
+	{"BH_Fuselage6",DIFFUSE,			"uh60l_fuselage_06", 			true};
+	{"BH_Fuselage6",ROUGHNESS_METALLIC, "uh60l_fuselage_06_RoughMet",	true};
+
+	{"BH_Fuselage7",DIFFUSE,			"uh60l_fuselage_07", 			true};
+	{"BH_Fuselage7",ROUGHNESS_METALLIC, "uh60l_fuselage_07_RoughMet",	true};
+	{"BH_Fuselage7",1,					"uh60l_fuselage_07_Normal",		true};
+
+	{"BH_ESS",DIFFUSE,					"uh60l_ess",					true};
+	{"BH_ESS",ROUGHNESS_METALLIC, 		"uh60l_ess_RoughMet",			true};
+	{"BH_tank230",DIFFUSE,				"uh60l_tank230",				true};
+	{"BH_tank230",ROUGHNESS_METALLIC, 	"uh60l_tank230_RoughMet",		true};
+	{"BH_tank230",NORMAL_MAP,			"uh60l_tank230_Normal",			true};
+	{"blades",DIFFUSE,					"uh60l_blades",					true};
+	{"blades",ROUGHNESS_METALLIC, 		"uh60l_blades_RoughMet",		true};
+	{"BH_Rotors",DIFFUSE,				"uh60l_mainrotor",				true};
+	{"BH_Rotors",ROUGHNESS_METALLIC, 	"uh60l_mainrotor_RoughMet",		true};
+}
+
+name =  "C Co. 2-158 'Crazy Hawk' "
+countries = {"USA"}
